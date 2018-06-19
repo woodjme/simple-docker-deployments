6a62bde524316a4ce53bef0a55c037c0b3d6a00f
Jamie Wood <me@jamiewood.io>
2018-06-19 21:19:21 +0100

    Initial Commit

---
+++ index.js
@@ -0,0 +1,118 @@
+
+const express = require('express');
+const app = express();
+const port = process.env.port || 8085;
+const bodyParser = require('body-parser');
+const Docker = require('dockerode');
+const docker = new Docker({
+	socketPath: '/var/run/docker.sock'
+});
+
+app.use(bodyParser.json());
+
+//Log Requests
+app.use(function (req, res, next) {
+	console.log(req.path);
+	next();
+});
+
+app.get('/', (req, res) => {
+	res.send('Hello');
+});
+
+app.post('/deploy', (req, res) => {
+	if (!req.body.service.name || !req.body.service.image) {
+		res.status(400);
+		res.send('Service not defined');
+	} else {
+		stopAndRemove(req.body.service.name)
+			.then(function () {
+				return pullImage(req.body.service.image)
+			})
+			.then(function () {
+				return startNewContainer(req.body.service.name, req.body.service.image)
+			})
+			.then(function(container) {
+				console.log(container.id)
+				res.sendStatus(200);
+			})
+			.catch(function (err) {
+				console.log(err)
+				res.sendStatus(500)
+			})
+
+	}
+
+});
+
+app.listen(port, () => {
+	console.log(`API app listening at ${port}`);
+});
+
+
+function stopAndRemove(serviceName) {
+	return new Promise((resolve, reject) => {
+		docker.listContainers()
+			.then(function (containers) {
+				if (containers.some(e => (e.Names.includes(`/${serviceName}`)))) {
+					docker.getContainer(serviceName)
+						.stop()
+						.then(function (container) {
+							console.log(`${serviceName} stopped`)
+							return (container.remove());
+						}).then(function () {
+							console.log(`${serviceName} removed`)
+							resolve()
+						}).catch(function (err) {
+							reject(err)
+						});
+				} else {
+					console.log('First time we have seen this image')
+					resolve()
+				}
+			})
+
+	})
+}
+
+function startNewContainer(serviceName, image) {
+	return new Promise((resolve, reject) => {
+		console.log('Starting new container...')
+		docker.createContainer({
+				Image: image,
+				AttachStdin: false,
+				AttachStdout: false,
+				AttachStderr: false,
+				Tty: false,
+				Cmd: [],
+				OpenStdin: false,
+				StdinOnce: false,
+				name: serviceName
+			}).then(function (container) {
+				return container.start()
+			})
+			.then(function (container) {
+				resolve(container)
+			})
+			.catch(function (err) {
+				reject(err)
+			})
+
+	})
+}
+
+function pullImage(image) {
+	return new Promise((resolve, reject) => {
+		console.log(`Pulling image ${image}...`)
+		docker.pull(image, (err, stream) => {
+			docker.modem.followProgress(stream, onFinished);
+
+			function onFinished(err) {
+				if (err) reject(err)
+				console.log(`Image ${image} pulled`)
+				resolve()
+			}
+		});
+	})
+}
+
---
+++ readme.md
@@ -0,0 +1,64 @@
+# Simple Docker Deploy
+
+A stripped down barebones docker deployment service enabling deployments of built docker images available in public docker repositories to a single docker host.
+
+*This project should not be used in a production environment!*
+
+## Installation
+
+Needs to be installed on a machine that has Docker installed and running.
+
+* Clone repo
+* `npm install`
+* `node index.js`
+
+Would be a good idea to use a process manager such as [pm2](http://pm2.keymetrics.io) to keep the service running in the background.
+
+### ToDo
+
+* Run within Docker
+
+## Usage
+
+Send a `PUT` request to the web server with a `JSON` body detailed below.
+
+```json
+{
+    "service": {
+        "name": "nginx",
+        "image": "nginx:latest"
+    }
+}
+```
+
+## Example
+
+### Request
+
+#### Route
+
+`http://localhost:8085/deploy`
+
+#### Body
+
+```json
+{
+    "service": {
+        "name": "nginx",
+        "image": "nginx:latest"
+    }
+}
+```
+
+### Output
+
+```zsh
+API app listening at 8085
+/deploy
+nginx stopped
+nginx removed
+Pulling image nginx:latest...
+Image nginx:latest pulled
+Starting new container...
+ec0ac0a7314e9853143f5508c8b9dc3cf6a2b6785790dedb16eaee83cf6cba82
+```
